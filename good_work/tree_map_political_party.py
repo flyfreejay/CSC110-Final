194d9fe8b52c478dd60866de60b34319ffb9e4e1
AndytheCreator223 <95309657+AndytheCreator223@users.noreply.github.com>
2021-12-10 17:25:46 -0500

    Update tree_map_political_party.py

--- good_work/tree_map_political_party.py
+++ good_work/tree_map_political_party.py
@@ -1,16 +1,16 @@
-import plotly.express as px
-import pandas as pd
-from bubble_map_with_color_and_scale import process_row
-
-
-
-df_hate_crime = pd.read_csv('../data/state_colour_data.csv')
-df_state_colour = pd.read_csv('../data/state_colour_data.csv')
-
-df_hate_crime['Colour']= df_hate_crime['Population']
-for row in range(len(df_hate_crime)):
-    df_hate_crime['Colour'][row] = df_state_colour[]
-
-fig= px.treemap()
-
-
+# import plotly.express as px
+# import pandas as pd
+# from bubble_map_with_color_and_scale import process_row
+#
+#
+#
+# df_hate_crime = pd.read_csv('../data/state_colour_data.csv')
+# df_state_colour = pd.read_csv('../data/state_colour_data.csv')
+#
+# df_hate_crime['Colour']= df_hate_crime['Population']
+# for row in range(len(df_hate_crime)):
+#     df_hate_crime['Colour'][row] = df_state_colour[]
+#
+# fig= px.treemap()
+#
+#
