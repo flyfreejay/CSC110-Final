ed490823537fe419b5f7c1d3619525cb519fb6b0
jaylee3 <flyfreejay@icloud.com>
2021-12-10 20:08:11 -0500

    I fucked around and ended up commmenting most of the functions I wrote cuz they are meaningless lmaoo

--- good_work/bar_graph.py
+++ good_work/bar_graph.py
@@ -18,6 +18,7 @@ import pandas as pd
 import plotly.express as px
 
 hate_crime_data = pd.read_csv('../data/hate_crime_data.csv')
+state_colour_data = pd.read_csv('../data/state_colour_data.csv')
 
 
 def draw_anti_asian_comparison() -> None:
@@ -28,11 +29,40 @@ def draw_anti_asian_comparison() -> None:
     fig.update_layout(barmode='group')
     fig.update_traces(marker_line_width=0)
     fig.update_layout(
-        title="Plot Title",
+        title="2019 Anti-Asian Hatecrimes vs 2020 Anti-Asian Hatecrimes",
         xaxis_title="US State",
         yaxis_title="Number of Hatecrime Cases",
     )
     fig.show()
+
+
+# def draw_aapi_to_anti_asian_hatecrimes2019() -> None:
+#     """
+#     """
+#     fig = px.bar(hate_crime_data, x="US State", y=['2019 Anti-Asian', '% of Population-AAPI'])
+#     fig.update_layout(barmode='group')
+#     fig.update_traces(marker_line_width=0)
+#     fig.update_layout(
+#         title="Correlation between ",
+#         xaxis_title="US State",
+#         yaxis_title="Number of Hatecrime Cases",
+#     )
+#     fig.show()
+
+# def draw_total_hatecrime_to_anti_asian_hatecrime_2019() -> None:
+#     """Extract 'US State' column from hate_crime_data.csv and draw a bar-graph
+#     comparing the numbers of Asian hate crimes to total hate crime numbers
+#     """
+#     fig = px.bar(hate_crime_data, x="US State", y=['Total HateCrimes 2019', '2019 Anti-Asian'])
+#     fig.update_layout(barmode='group')
+#     fig.update_traces(marker_line_width=0)
+#     fig.update_layout(
+#         title="Total Hatecrimes vs Anti-Asian Hatecrimes in 2019",
+#         xaxis_title="US State",
+#         yaxis_title="Number of Hatecrime Cases",
+#     )
+#     fig.show()
+
 # change the directory path if this file's location changes
 
 # if __name__ == '__main__':
