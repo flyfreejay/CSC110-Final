0e39af5b9e3d2517f8bec91eaddf64aae9af58d3
AndytheCreator223 <95309657+AndytheCreator223@users.noreply.github.com>
2021-12-11 21:13:45 -0500

    update treemap

--- good_work/tree_map_political_party_old.py
+++ good_work/tree_map_political_party_old.py
@@ -15,6 +15,7 @@ This file is Copyright (c) 2021 Jay Lee, Andy Feng, and Jamie Yi
 import plotly.express as px
 import pandas as pd
 import csv
+from process_hate_crime_csv import process_hate_crime_csv
 
 
 def read_csv_file_return_dict(filename: str) -> dict[str, str]:
@@ -46,21 +47,18 @@ def draw_total_aapi_hate_crime_2020_tree_map() -> None:
     tree map represents the total number of AAPI hate crimes reported in 2020 for that area/region.
 
     """
-    df_hate_crime = pd.read_csv('../data/hate_crime_data.csv')
-    dic = read_csv_file_return_dict('../data/state_colour_data.csv')
-
-    df_hate_crime['colour'] = df_hate_crime['Population']
+    df_hate_crime = process_hate_crime_csv()
     df_hate_crime['party'] = df_hate_crime['Population']
 
     for row in range(len(df_hate_crime)):
-        df_hate_crime['colour'][row] = dic[df_hate_crime['US State'][row]]
         if df_hate_crime['colour'][row] == 'crimson':
             df_hate_crime['party'][row] = 'Republican'
         else:
             df_hate_crime['party'][row] = 'Democratic'
 
     fig = px.treemap(df_hate_crime, path=[px.Constant('USA'), 'party', 'US State', 'US City'],
-                     values='Total Hate Crimes 2020', color='colour',
-                     title='Total Number of AAPI Hate Crimes in United States, 2020')
+                     values='size', color='colour',
+                     title='Total Percentage Increase of AAPI Hate Crimes in Major US Cities, '
+                           '2019-2020')
     fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
     fig.show()
